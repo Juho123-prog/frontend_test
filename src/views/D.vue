140d8880b8e3478556ebd65c85fe01d4cfc66899
Juho123-prog <juhobag12@gmail.com>
2026-01-13 09:26:47 +0900

    vuefinder 버전 수정

--- src/views/D.vue
+++ src/views/D.vue
@@ -1,14 +1,35 @@
 <template>
-  <vue-finder 
-    id="my_vuefinder"
-    :driver="driver"
-  />
+    <vue-finder
+      id='my_vuefinder'
+      :request="request"
+      :max-file-size="maxFileSize"
+    />
 </template>
 
 <script setup>
-import { RemoteDriver } from 'vuefinder';
+import {getCurrentInstance, ref} from 'vue';
 
-const driver = new RemoteDriver({
-  baseURL: 'https://autox.dreamai.kr/vuefinder_api/?additionalParam1=yes&q=index&adapter=lab-aimed_directory&vf=1'
-})
+const request = {
+  // ----- CHANGE ME! -----
+  // [REQUIRED] Url for development server endpoint
+  // baseUrl: "http://localhost:8005",
+  // baseUrl: "https://autox.dreamai.kr/vuefinder_api",
+  baseUrl: "https://autox.dreamai.kr/vuefinder_api/",
+  headers: { "X-ADDITIONAL-HEADER": 'yes' },
+  params: { additionalParam1: 'yes' },
+  body: { additionalBody1: ['yes'] },
+  // And/or transform request callback
+  transformRequest: req => {
+    if (req.method === 'get') {
+      req.params.vf = "1"
+      req.params.adapter = "lab-aimed_directory"
+      // req.params.adapter = "mansu" + "_directory"
+    }
+    return req;
+  },
+  // XSRF Token header name
+  xsrfHeaderName: "CSRF-TOKEN",
+}
+const maxFileSize = ref("100000MB")
 </script>
++
